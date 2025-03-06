880878143372ea997f4e13e3c8c4e49ccd7cbbe4
Rafael-Sabatini <bonieliasabatini@gmail.com>
2025-03-06 08:28:38 -0300

    Ajustes github

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/BitControl/",
+  base: "/bit-control/",
   plugins: [react(), tailwindcss()],
 });
